44dffb5019f6a833b1a1ffa81f02c825fe8efb92
NickPaul <nwpaul@mednet.ucla.edu>
2022-04-12 09:15:01 -0500

    Added A pooled Connection Factory.

--- src/main/java/me/nickpaul/SimpleRoute.java
+++ src/main/java/me/nickpaul/SimpleRoute.java
@@ -4,17 +4,24 @@ import javax.enterprise.context.ApplicationScoped;
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.sjms2.Sjms2Component;
+import org.messaginghub.pooled.jms.JmsPoolConnectionFactory;
 
 @ApplicationScoped
 public class SimpleRoute extends RouteBuilder {
 
   @Override
   public void configure() throws Exception {
+    // Create the JMS Connection Factory
     ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
     activeMQConnectionFactory.setBrokerURL("tcp://localhost:61616");
 
+    // Create the pooled connection
+    JmsPoolConnectionFactory pooledConnectionFactory = new JmsPoolConnectionFactory ();
+    pooledConnectionFactory.setConnectionFactory(activeMQConnectionFactory);
+
+    // Create a unique component with the pooled connection.
     Sjms2Component component = new Sjms2Component();
-    component.setConnectionFactory(activeMQConnectionFactory);
+    component.setConnectionFactory(pooledConnectionFactory);
     this.getCamelContext().addComponent("my-sjms2", component);
 
 
